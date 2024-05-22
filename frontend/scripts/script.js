8c916aea6ee98ceb92a8ae5025fbd41dcdb4f108
AndreasDebel <andreasdebel@hotmail.com>
2024-05-22 13:32:29 +0200

    indsæt knapper automatisk

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,3 +1,4 @@
+// Det første dilemma har tre knapper, som skal ændre noget content afhængig af hvad du trykker på
 document.getElementById('dillema1Knap1').addEventListener('click', function() {
     document.getElementById('dynamicContent').innerText = "Du valgte mulighed 1. Her er det tilsvarende indhold.";
 });
@@ -10,11 +11,11 @@ document.getElementById('dillema1Knap3').addEventListener('click', function() {
     document.getElementById('dynamicContent').innerText = "Du valgte mulighed 3. Her er det tilsvarende indhold.";
 });
 
-
+// Denne variable fortæller, hvilken main brugeren sidst har bevæget sig hen til m. knapperne
 let currentSection = 0
 console.log("Current section is " + currentSection);
 
-// Funktionen er ændret, så den tager højde for variablen currentSection, og så sender den dig til næste main
+// Funktionen tager højde for variablen currentSection, og så sender den dig til næste main
 function scrollForward() {
     // Find den ønskede sektion
     let section = document.getElementById('main'+(currentSection+1));
@@ -25,15 +26,6 @@ function scrollForward() {
     section.scrollIntoView({behavior: 'smooth'});
     
 }
-
-// function scrollToSection() {
-//     // Find den ønskede sektion
-//     let section = document.getElementById('main1');
-    
-//     // Brug scrollIntoView-metoden for at scrolle til sektionen
-//     section.scrollIntoView({behavior: 'smooth'});
-// }
-
 
 // Funktionen sender dig til forrige main
 function scrollBack() {
@@ -56,3 +48,48 @@ function scrollToTop() {
     prevSection.scrollIntoView({behavior: 'smooth'});
     };
 }
+
+// Sæt knapper ind i alle .main .header
+d3.selectAll('.main .header')
+    .each(function(d, i) {
+
+        const numOfHeaders = d3.selectAll('.main .header').size();
+        if(i !== 0) {
+            d3.select(this)
+                .append('button')
+                    .attr('class', 'backbtn')
+                    .attr('onclick', 'scrollBack()')
+                    .text('Forrige side');
+            d3.select(this)            
+                .append('img')
+                    .attr('class', 'arrowBack')
+                    .attr('src', 'Images/arrow-small-left.png')
+                    .attr('alt', ''); 
+        }
+
+        if(i !== numOfHeaders - 1) {
+            d3.select(this)
+                .append('button')
+                    .attr('class', 'forwardbtn')
+                    .attr('onclick', 'scrollForward()')
+                    .text('Næste side');
+            d3.select(this)
+                .append('img')
+                    .attr('class', 'arrowDown')
+                    .attr('src', 'Images/arrow-small-left.png')
+                    .attr('alt', '');
+        }
+    
+        if(i !== 0) {
+            d3.select(this)
+                .append('button')
+                    .attr('class', 'topbtn')
+                    .attr('onclick', 'scrollToTop()')
+                    .text('Til toppen');
+                d3.select(this)
+                .append('img')
+                    .attr('class', 'arrowHome')
+                    .attr('src', 'Images/angle-double-small-up.png')
+                    .attr('alt', '');
+        }
+    });
